948c3dba9a817483b120bcf66c6b80f49068dcd3
Elizabeth K. G-S <81371428+lizodus@users.noreply.github.com>
2022-04-06 01:17:42 -0700

    Update Exchange.sol

--- packages/hardhat/contracts/Exchange.sol
+++ packages/hardhat/contracts/Exchange.sol
@@ -9,26 +9,37 @@ import "./Nestcoin.sol";
 contract Nxt {
     NestCoin public nestcoin;
 
+    event Payment(address indexed payer, uint amount, string indexed ref);
+
     constructor(address tokenAddr) {
         nestcoin = NestCoin(tokenAddr);
     }
-
 
     function batchTokenTransfer(address owner, address[] memory _userAddr,  uint256[] memory _amount) public  {
         require(_userAddr.length == _amount.length, "Number of Addresses must match amount");
         for (uint256 i = 0; i < _userAddr.length; i++) {
             transferFrom(owner, _userAddr[i], _amount[i]);
         }
+    }
+
+    // with a ref, every payment is traceable to the value provided
+    function pay(uint amountOfTokens, string ref) public {
+
+
+        // Transfer token
+        nestcoin.transferFrom(msg.sender, address(this), amountOfTokens);
+
+        // Emit Pay event
+        emit Payment(msg.sender, amountOfTokens, ref);
 
     }
 
     function triggerMint() public onlyOwner {
-        NestCoin.mint()
+        nestCoin.mint(_msgSender(), amount);
     }
 
     function burn(uint256 amount) public {
         _burn(_msgSender(), amount);
     }
-
 }
 
